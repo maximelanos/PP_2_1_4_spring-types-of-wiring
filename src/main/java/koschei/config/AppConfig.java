68142d49e27c2364af9b25615609dd649d22babf
Maksim Bardikov <maximelanosx@yandex.ru>
2023-08-05 13:34:40 +0300

    task ready

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,7 @@
 package koschei.config;
 
+import koschei.models.Deth8;
+import koschei.models.Egg6;
 import koschei.models.Island2;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
@@ -13,5 +15,10 @@ public class AppConfig {
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+
+    @Bean
+    public static Egg6 getEgg(Deth8 deth) {
+        return new Egg6(deth);
     }
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Egg6 {
+
+    private Deth8 deth;
+
+    public Egg6(Deth8 deth) {
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + deth.toString();
     }
 }
